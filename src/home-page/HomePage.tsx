34be63d878a7310f22b693459e518dead382ec7d
realcryptogates <dksd93@gmail.com>
2021-06-16 08:05:02 +0900

    grubdash homepage

---
+++ src/home-page/HomePage.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import '../index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import img1 from './ScootScoot.svg';
+import img2 from './iphone.svg';
+import img3 from './Storefront.svg';
+
+type Props = {
+}
+
+const HomePage: React.FC<Props> = (props) => {
+  return (
+    <>
+      <div className="banner">
+        <div>
+          <input className="topButton" value="Sign-Up" type='submit'/>
+          <h1 className='whiteText'>GrubDash</h1><br/>
+        </div>
+        <br/>
+        <h2 className='whiteText'>Restaurants and more, </h2>
+        <h2 className='whiteText'>delivered to your door</h2>
+        <br/>
+        <input className="input1"/> <input value="Go" className="input2" type='submit'/>
+      </div>
+      <div className="row">
+        <div className="col-sm-4">
+          <div className='imgbox'>
+            <img src={ img1 } alt='somthing' className="image1"/>
+            <h3>Become a Dasher</h3>
+          </div>
+        </div>
+        <div className="col-sm-4">
+          <div className='imgbox'>
+            <img src={ img2 } alt='somthing' className="image1"/>
+            <h3>Try the app</h3>
+          </div>
+        </div>       
+        <div className="col-sm-4">
+          <div className='imgbox'>
+            <img src={ img3 } alt='somthing' className="image1"/>
+            <h3>View Restuarants</h3>
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
---
+++ src/restaurants/all-restaurants-page/AllRestaurantsPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type Props = {
+
+}
+
+const AllRestaurantsPage: React.FC<Props> = (props) => {
+
+  return (
+    <>
+      <p>All Restaurants Page</p>
+      adsfasdf
+    </>
+  );
+};
+
+export default AllRestaurantsPage;
